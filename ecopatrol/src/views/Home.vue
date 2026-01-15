123020d90b07b62365f3adf3b19fe42c99d2e8f7
Volodya Sargsyan <volsarg@gmail.com>
2026-01-15 17:08:58 +0400

    check changes

--- ecopatrol/src/views/Home.vue
+++ ecopatrol/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="home">
+  <div class="home mt-5">
     <div class="hero is-medium">
-      <div class="hero-body has-text-centered">
+      <div class="has-text-centered pt-5">
         <h1 class="title">Welcome to Eco-patrol</h1>
         <h2 class="subtitle">An online place for searching what you want</h2>
       </div>
@@ -17,9 +17,9 @@
               <i class="far fa-clock"></i>
             </span>
             <h2 class="is-size-4 mt-4 mb-4">
-              Eco-patrol service employees
+              Էկոպարեկային ծառայության աշխատակիցներ
             </h2>
-            <p>This is just some random placeholder text</p>
+            <p>Ներկա պահին գործող</p>
           </div>
         </div>
 
@@ -29,27 +29,27 @@
               <i class="far fa-comments"></i>
             </span>
             <h2 class="is-size-4 mt-4 mb-4">
-              People who made the bet
+              Էկոպարեկային ծառայության դիմորտներ
             </h2>
-            <p>This is just some random placeholder text</p>
+            <p>Դիմել են կամ դեռ ընթացքի մեջ է</p>
           </div>
         </div>
 
         <div class="column is-4">
-          <div class="box has-text-centered">
+          <div class="box has-text-centered" href="/log-in">
             <span class="icon is-size-2 is-size-info">
               <i class="far fa-home"></i>
             </span>
             <h2 class="is-size-4 mt-4 mb-4">
-              The machines that made the draw
+              Էկոպարեկային ծառայության նախքին աշխատակիցներ
             </h2>
-            <p>This is just some random placeholder text</p>
+            <p>Հեռացված կամ դիմումին համաձայն ազատված</p>
           </div>
         </div>
         
         <div class="column is-12 has-text-centered">
           <a href="/log-in" class="button is-size-3 mt-6 mb-6" style="background: #163820; color:#ffffff;">
-            Click to get starte
+            Մուտք գքրծել համակարգ
           </a>
         </div>
       </div>
--- ecopatrol/src/views/dashboard/Dashboard.vue
+++ ecopatrol/src/views/dashboard/Dashboard.vue
@@ -3,125 +3,62 @@
         <div class="columns is-multiline">
             <div class="column is-12">
                 <section class="section">
-                    <div class="container">
-                        <div class="columns is-multiline">
-                            <div class="column is-4">
-                                <div class="box">
-                                    <div class="has-text-centered">
-                                         <h2 class="is-size-4 mt-4 mb-4">
-                                            Հայաստանի Հանրապետության քաղաքացի
-                                        </h2>
-                                    </div>
-
-                                    <form action="" v-on:submit.prevent="personSearch">
-                                        <div class="field">
-                                            <label for="">Անուն</label>
-                                            <div class="control">
-                                                <input type="text" class="input" v-model="person.first_name"/>
-                                            </div>
-                                        </div>
-
-                                        <div class="field">
-                                            <label for="">Ազգանուն</label>
-                                            <div class="control">
-                                                <input type="text" class="input" v-model="person.last_name"/>
-                                            </div>
-                                        </div>
-
-                                        <div class="field">
-                                            <label for="">Անձնագիր</label>
-                                            <div class="control">
-                                                <input type="text" class="input" v-model="person.passport"/>
-                                            </div>
-                                        </div>
-
-                                        <div class="field">
-                                            <label for="">Նույնականացման քարտ</label>
-                                            <div class="control">
-                                                <input type="password" class="input" v-model="person.id_card"/>
-                                            </div>
-                                        </div>
-
-                                        <div class="field">
-                                            <label for="">ՀՎՀՀ</label>
-                                            <div class="control">
-                                                <input type="password" class="input" v-model="person.hvhh"/>
-                                            </div>
-                                        </div>
-
-                                        <div class="notification is-danger" v-if="errors.length">
-                                            <p v-for="error in errors" v-bind:key="erre">
-                                                {{ error }}
-                                            </p>
-                                        </div>
-
-                                        <div class="field">
-                                            <div class="control">
-                                                <button class="button" style="background: #163820; color: #ffffff;">Փնտրել</button>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
+                    <div class="container columns is-multiline column is-5">
+                        <div class="box">
+                            <div class="has-text-centered">
+                                    <h2 class="is-size-4 mt-4 mb-4">
+                                    Հայաստանի Հանրապետության քաղաքացի
+                                </h2>
                             </div>
 
-                            <div class="column is-4">
-                                <div class="box">
-                                    <div class="has-text-centered">
-                                         <h2 class="is-size-4 mt-4 mb-4">
-                                            Խախտումների վերաբերյալ տեղեկություններ
-                                        </h2>
+                            <form action="" v-on:submit.prevent="personSearch">
+                                <div class="field">
+                                    <label for="">Անուն</label>
+                                    <div class="control">
+                                        <input type="text" class="input" v-model="person.first_name"/>
                                     </div>
-
-                                    <form action="" v-on:submit.prevent="submitForm">
-                                        <div class="field">
-                                            <label for="">Անուն</label>
-                                            <div class="control">
-                                                <input type="text" class="input" v-model="first_name"/>
-                                            </div>
-                                        </div>
-
-                                        <div class="field">
-                                            <label for="">Ազգանուն</label>
-                                            <div class="control">
-                                                <input type="text" class="input" v-model="last_name"/>
-                                            </div>
-                                        </div>
-
-                                        <div class="field">
-                                            <label for="">Անձնագիր</label>
-                                            <div class="control">
-                                                <input type="text" class="input" v-model="passport"/>
-                                            </div>
-                                        </div>
-
-                                        <div class="field">
-                                            <label for="">Նույնականացման քարդ</label>
-                                            <div class="control">
-                                                <input type="password" class="input" v-model="id_card"/>
-                                            </div>
-                                        </div>
-
-                                        <div class="field">
-                                            <label for="">ՀՎՀՀ</label>
-                                            <div class="control">
-                                                <input type="password" class="input" v-model="hvhh"/>
-                                            </div>
-                                        </div>
-
-                                        <div class="notification is-danger" v-if="errors.length">
-                                            <p v-for="error in errors" v-bind:key="erre">
-                                                {{ error }}
-                                            </p>
-                                        </div>
-
-                                        <div class="field">
-                                            <div class="control">
-                                                <button class="button" style="background: #163820; color: #ffffff;">Փնտրել</button>
-                                            </div>
-                                        </div>
-                                    </form>
                                 </div>
-                            </div>
+
+                                <div class="field">
+                                    <label for="">Ազգանուն</label>
+                                    <div class="control">
+                                        <input type="text" class="input" v-model="person.last_name"/>
+                                    </div>
+                                </div>
+
+                                <div class="field">
+                                    <label for="">Անձնագիր</label>
+                                    <div class="control">
+                                        <input type="text" class="input" v-model="person.passport"/>
+                                    </div>
+                                </div>
+
+                                <div class="field">
+                                    <label for="">Նույնականացման քարտ</label>
+                                    <div class="control">
+                                        <input type="password" class="input" v-model="person.id_card"/>
+                                    </div>
+                                </div>
+
+                                <div class="field">
+                                    <label for="">ՀՎՀՀ</label>
+                                    <div class="control">
+                                        <input type="password" class="input" v-model="person.hvhh"/>
+                                    </div>
+                                </div>
+
+                                <div class="notification is-danger" v-if="errors.length">
+                                    <p v-for="error in errors" v-bind:key="erre">
+                                        {{ error }}
+                                    </p>
+                                </div>
+
+                                <div class="field">
+                                    <div class="control">
+                                        <button class="button" style="background: #163820; color: #ffffff;">Փնտրել</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </section>
